5abde39456b2415cd0762ceb093aea013c94e1c9
Slmply <26casey.groff@wfbschools.com>
2024-03-02 21:02:16 -0600

    Added Aesprite Wizard and Started Work on Player Art

--- Module/GameManager/GameManager.cs
+++ Module/GameManager/GameManager.cs
@@ -26,7 +26,7 @@ public partial class GameManager : Node2D
 	private StockManager stockManager;
 	private Timer gameTick;
 
-    public override void _Ready()
+	public override void _Ready()
 	{
 		stockManager = GetNode<StockManager>("Stock Manager");
 		stockManager.updateStocks(0.0f);
@@ -43,19 +43,22 @@ public partial class GameManager : Node2D
 		stockManager.updateStocks(currentTime);
 	}
 
-	public void activateStockScreen() {
+	public void activateStockScreen()
+	{
 		stockManager.toggleStockInfo();
 		Visible = false;
 	}
 
-	public void disableStockScreen() {
+	public void disableStockScreen()
+	{
 		stockManager.hideStockInfo();
 		Visible = true;
 	}
 
-	
 
-	public static string timeToHour(double time) {
+
+	public static string timeToHour(double time)
+	{
 
 		string res = "";
 
@@ -63,14 +66,18 @@ public partial class GameManager : Node2D
 
 		time = time * 24.0;
 
-		if (time >= 12.0) {
-			res += "PM";
-		} else {
-			res += "AM";
+		if (time >= 12.0)
+		{
+			res += " PM";
+		}
+		else
+		{
+			res += " AM";
 		}
 
-		time =  (time % 12);
-		if (time < 1) {
+		time = (time % 12);
+		if (time < 1)
+		{
 			time += 12;
 		}
 		double mins = time % 1;
--- Module/GameManager/PlayerHud.cs
+++ Module/GameManager/PlayerHud.cs
@@ -9,16 +9,16 @@ public partial class PlayerHud : CanvasLayer
 	private Label timeLabel;
 	private Label moneyLabel;
 
-    public override void _Ready()
-    {
+	public override void _Ready()
+	{
 		timeLabel = GetNode<Label>("TimeMoneyContainer/VBoxContainer/Time");
 		moneyLabel = GetNode<Label>("TimeMoneyContainer/VBoxContainer/Money");
-    }
+	}
 
 
-    public override void _Process(double delta)
-    {
-    	timeLabel.Text = GameManager.timeToHour(gameManager.currentTime);
+	public override void _Process(double delta)
+	{
+		timeLabel.Text = GameManager.timeToHour(gameManager.currentTime).ToLower();
 		moneyLabel.Text = gameManager.money.ToString();
-    }
+	}
 }
